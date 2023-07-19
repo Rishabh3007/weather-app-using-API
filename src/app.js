bfc3787840fd4677111794ca4d13f6887fb42970
Rishabh Jain <jainrishabh3007@gmail.com>
2023-07-19 19:29:19 +0530

    2nd commit-a modified app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+const publicDirectoryPath = path.join(__dirname,"../public");
+app.use(express.static(publicDirectoryPath));
 
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    res.send('Welcome to the Home Page');
 });
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000');
+app.get('/about',(req,res)=>{
+    res.send('Welcome to the about Page');
+});
+
+app.get('/weather',(req,res)=>{
+    res.send('Welcome to the weather Page');
+});
+
+app.get('*',(req,res)=>{
+    res.send('ERROR 404: Opps Page Cannot be found');
+});
+
+app.listen(port,()=>{
+    console.log(`Server is up on port ${port}`);
 });
